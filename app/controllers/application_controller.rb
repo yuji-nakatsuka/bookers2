ec0ec5505d6266ebe7bcc80d6598a8a13c88b96a
yuji-nakatsuka <u.z.aka.dj0ma@gmail.com>
2019-07-16 11:58:08 +0000

    [start]5-9

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+
+
+  def after_sign_in_path_for(resource)
+  	user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+  	root_path
+  end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
+
+end
