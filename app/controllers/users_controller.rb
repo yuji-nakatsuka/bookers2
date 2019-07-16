6ee135f40846a54865bf8735b1eca0ca7467e0e7
yuji-nakatsuka <u.z.aka.dj0ma@gmail.com>
2019-07-16 12:49:24 +0000

    5-9-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,19 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update (user_params)
+  	redirect_to user_path(current_user)
+  end
+
+private
+def user_params
+	params.require(:user).permit(:name, :profile_image, :introduction)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root'users#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :index, :top]
+  resources :users, only: [:show, :index, :top, :edit, :update]
 end
